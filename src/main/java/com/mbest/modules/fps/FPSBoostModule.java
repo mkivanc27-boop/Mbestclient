51c4f9933fda73c835318fa6b8066cf723fa3f1c
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-06 00:37:39 +0300

    Update FPSBoostModule.java

--- src/main/java/com/mbest/modules/fps/FPSBoostModule.java
+++ src/main/java/com/mbest/modules/fps/FPSBoostModule.java
@@ -19,9 +19,7 @@ public class FPSBoostModule extends Module {
     }
 
     @Override
-    public void onEnable() {
-        apply();
-    }
+    public void onEnable() { apply(); }
 
     public void setLevel(BoostLevel level) {
         this.level = level;
@@ -38,27 +36,24 @@ public class FPSBoostModule extends Module {
             case LOW -> {
                 mc.options.getViewDistance().setValue(6);
                 mc.options.getGraphicsMode().setValue(GraphicsMode.FAST);
-                mc.options.getSmoothLighting().setValue(false);
                 mc.options.getEntityDistanceScaling().setValue(0.75);
-                mc.options.getCloudRenderMod().setValue(CloudRenderMode.FAST);
+                mc.options.getCloudsMode().setValue(CloudRenderMode.FAST);
                 mc.options.getParticles().setValue(ParticlesMode.DECREASED);
                 mc.options.getMaxFps().setValue(260);
             }
             case MEDIUM -> {
                 mc.options.getViewDistance().setValue(5);
                 mc.options.getGraphicsMode().setValue(GraphicsMode.FAST);
-                mc.options.getSmoothLighting().setValue(false);
                 mc.options.getEntityDistanceScaling().setValue(0.5);
-                mc.options.getCloudRenderMod().setValue(CloudRenderMode.OFF);
+                mc.options.getCloudsMode().setValue(CloudRenderMode.OFF);
                 mc.options.getParticles().setValue(ParticlesMode.MINIMAL);
                 mc.options.getMaxFps().setValue(260);
             }
             case HIGH -> {
                 mc.options.getViewDistance().setValue(4);
                 mc.options.getGraphicsMode().setValue(GraphicsMode.FAST);
-                mc.options.getSmoothLighting().setValue(false);
                 mc.options.getEntityDistanceScaling().setValue(0.25);
-                mc.options.getCloudRenderMod().setValue(CloudRenderMode.OFF);
+                mc.options.getCloudsMode().setValue(CloudRenderMode.OFF);
                 mc.options.getParticles().setValue(ParticlesMode.MINIMAL);
                 mc.options.getBiomeBlendRadius().setValue(0);
                 mc.options.getMaxFps().setValue(260);
@@ -66,13 +61,12 @@ public class FPSBoostModule extends Module {
             case ULTRA -> {
                 mc.options.getViewDistance().setValue(2);
                 mc.options.getGraphicsMode().setValue(GraphicsMode.FAST);
-                mc.options.getSmoothLighting().setValue(false);
                 mc.options.getEntityDistanceScaling().setValue(0.1);
-                mc.options.getCloudRenderMod().setValue(CloudRenderMode.OFF);
+                mc.options.getCloudsMode().setValue(CloudRenderMode.OFF);
                 mc.options.getParticles().setValue(ParticlesMode.MINIMAL);
                 mc.options.getBiomeBlendRadius().setValue(0);
-                mc.options.getMaxFps().setValue(260);
                 mc.options.getMipmapLevels().setValue(0);
+                mc.options.getMaxFps().setValue(260);
             }
         }
         mc.options.write();
@@ -84,9 +78,8 @@ public class FPSBoostModule extends Module {
         if (mc.options == null) return;
         mc.options.getViewDistance().setValue(8);
         mc.options.getGraphicsMode().setValue(GraphicsMode.FANCY);
-        mc.options.getSmoothLighting().setValue(true);
         mc.options.getEntityDistanceScaling().setValue(1.0);
-        mc.options.getCloudRenderMod().setValue(CloudRenderMode.FAST);
+        mc.options.getCloudsMode().setValue(CloudRenderMode.FAST);
         mc.options.getParticles().setValue(ParticlesMode.ALL);
         mc.options.getBiomeBlendRadius().setValue(2);
         mc.options.getMipmapLevels().setValue(4);
